596eff3864b474fa27bfb7c693e83dde5a5e6dcb
Jenny3005 <jennyhoundon@gmail.com>
2026-05-19 11:02:49 +0100

    Configuration CORS pour React

--- backend/settings.py
+++ backend/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -53,10 +52,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://localhost:3000",
-]
 ROOT_URLCONF = 'backend.urls'
 
 TEMPLATES = [
