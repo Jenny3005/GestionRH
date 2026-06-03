220c10ae6f7843f202903b35af40b1062d9c6147
Jenny3005 <jennyhoundon@gmail.com>
2026-06-03 18:17:13 +0100

    Ajout de l'import

--- monapp/views.py
+++ monapp/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.hashers import make_password, check_password
+from docx import Document
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
