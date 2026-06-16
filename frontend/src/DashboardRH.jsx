eaa2b9a4f89e870e54debd30068b608e95bfdd8b
Jenny3005 <jennyhoundon@gmail.com>
2026-06-16 09:20:13 +0100

    Correction du lien du statut de l'agent

--- frontend/src/DashboardRH.jsx
+++ frontend/src/DashboardRH.jsx
@@ -2422,7 +2422,7 @@ export default function DashboardRH() {
               <ul>
                 <li><a href="https://www.numerique.gouv.bj" target="_blank">Portail du Ministère</a></li>
                 <li><a href="https://eservices.travail.gouv.bj" target="_blank">E-Services SIGRH</a></li>
-                <li><a href="https://sgg.gouv.bj/doc/loi-2015-18/" target="_blank">Statut de l'Agent (SGG)</a></li>
+                <li><a href="https://sgg.gouv.bj/doc/loi-2015-018/" target="_blank">Statut de l'Agent (SGG)</a></li>
               </ul>
             </div>
             <div className="footer-col">
