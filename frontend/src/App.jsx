d77ffc288abcb31633ff68f1335aeb563ac7eee4
Jenny3005 <jennyhoundon@gmail.com>
2026-06-16 10:06:59 +0100

    Enelevement de Intra là

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -171,7 +171,7 @@ export default function App() {
         {/* BANDEAU HERO */}
         <section className="hero-banner-intranet">
           <div className="banner-content">
-            <h2>Bienvenue sur votre Portail Intra-RH</h2>
+            <h2>Bienvenue sur votre Portail </h2>
             <p>
               Retrouvez toutes les actualités administratives du Ministère, consultez les appels à candidatures internes 
               et accédez directement à vos outils de gestion de carrière.
