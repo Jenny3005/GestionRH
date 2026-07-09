df3aadc529934cb214f24ce3415ada8426648ca8
Jenny3005 <jennyhoundon@gmail.com>
2026-07-09 23:56:20 +0100

    essai pour regler le pb

--- backend/urls.py
+++ backend/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.urls import path, include, re_path  # ← AJOUTE re_path
 from django.views.generic import TemplateView  
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,3 +11,6 @@ urlpatterns = [
     re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
     
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
