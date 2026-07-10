c3dbb817d7a0c228808d25fbff3014a50e0aa85a
Jenny3005 <jennyhoundon@gmail.com>
2026-07-10 09:15:52 +0100

    Règlement des problèmes assets

--- backend/urls.py
+++ backend/urls.py
@@ -1,15 +1,19 @@
 from django.contrib import admin
-from django.urls import path, include
-from django.urls import path, include, re_path  # ← AJOUTE re_path
-from django.views.generic import TemplateView  
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('monapp.urls')), 
-    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
-    
+    path('api/', include('monapp.urls')),
+]
+
+urlpatterns += staticfiles_urlpatterns()
+
+urlpatterns += [
+    re_path(r'^(?!api/|static/|media/).*$', TemplateView.as_view(template_name='index.html')),
 ]
 
 if settings.DEBUG:
