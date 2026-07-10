da86ff684bda228fee896045b8e47e89b82c9683
Jenny3005 <jennyhoundon@gmail.com>
2026-07-10 09:15:52 +0100

    Règlement des problèmes assets

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/static/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+  },
   server: {
     port: 5173,
     hmr: {
