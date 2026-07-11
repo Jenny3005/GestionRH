5f38431a8ca2d1e43644258fcc743fa0df3510ac
Jenny3005 <jennyhoundon@gmail.com>
2026-07-11 16:34:25 +0100

    Essai d'amélioration pour eviter l'erreur

--- monapp/tests.py
+++ monapp/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.test import SimpleTestCase, override_settings
 
-# Create your tests here.
+from monapp.emails import resolve_email_backend
+
+
+class EmailBackendTests(SimpleTestCase):
+    @override_settings(EMAIL_HOST_USER='', EMAIL_HOST_PASSWORD='')
+    def test_uses_console_backend_when_smtp_credentials_missing(self):
+        self.assertEqual(resolve_email_backend(), 'django.core.mail.backends.console.EmailBackend')
+
+    @override_settings(EMAIL_HOST_USER='demo@example.com', EMAIL_HOST_PASSWORD='secret')
+    def test_uses_smtp_backend_when_credentials_present(self):
+        self.assertEqual(resolve_email_backend(), 'django.core.mail.backends.smtp.EmailBackend')
