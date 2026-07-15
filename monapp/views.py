7a4fcc1e089a1dbe8f6cdf82a88f0edf5f853792
Mary <maryshanad@gmail.com>
2026-07-15 01:05:19 +0100

    Refactor: Déploiement de Ollama

--- monapp/views.py
+++ monapp/views.py
@@ -4687,7 +4687,7 @@ def refresh_cached_analysis(matricule, refresh_payload=None):
 
 
 def _ensure_ollama_running():
-    host = os.getenv('OLLAMA_URL', 'https://linux-pen-reproduced-industrial.trycloudflare.com')
+    host = os.getenv('OLLAMA_URL', 'https://rocket-exhibit-visited-negotiation.trycloudflare.com')
     model_name = os.getenv('OLLAMA_MODEL', 'llama3.2:3b')
 
     try:
