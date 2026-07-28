698e60e340bcab9d0943bb60d7dfb333c505e489
Mary <maryshanad@gmail.com>
2026-07-28 15:10:29 +0100

    Mise à jour URL Cloudflare

--- monapp/views.py
+++ monapp/views.py
@@ -4687,7 +4687,7 @@ def refresh_cached_analysis(matricule, refresh_payload=None):
 
 
 def _ensure_ollama_running():
-    host = os.getenv('OLLAMA_URL', 'https://and-precious-andy-except.trycloudflare.com')
+    host = os.getenv('OLLAMA_URL', 'https://given-sentences-actions-governor.trycloudflare.com')
     model_name = os.getenv('OLLAMA_MODEL', 'llama3.2:3b')
 
     try:
